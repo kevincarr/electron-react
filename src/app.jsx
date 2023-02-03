5cae7e486bf1cb4522bf5f9992ed5cf34545674a
Kevin <kevinmcarr@hotmail.com>
2023-02-03 10:55:14 -0600

    Add Fullscreen and exit button

--- src/app.jsx
+++ src/app.jsx
@@ -3,9 +3,16 @@ import * as ReactDOMClient from 'react-dom/client';
 
 function App({ callback }) {
   // Callback will be called when the div is first created.
+  
+  const appQuit = (e) =>{ 
+    window.close();
+  }
   return (
     <div ref={callback}>
       <h2 className='text-red-600'>Hello from React in Electron!</h2>
+      <div id="exit-btn"
+        onMouseUp={appQuit.bind(this)}
+      >Exit</div>
     </div>
   );
 }
