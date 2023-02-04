83861749b4fa8696534f1d09a9e821187769e28b
Kevin <kevinmcarr@hotmail.com>
2023-02-04 16:01:50 -0600

    Add basic button component and functionality

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import { globals, setGlobals } from "./assets/Components/Shared/globals";
 import noteToSelf from "./assets/Components/Shared/noteToSelf";
+import ButtonPillBlue from "./assets/Components/Shared/Buttons/PillBlue";
 
 function App({ callback }) {
   // Callback will be called when the div is first created.
@@ -14,19 +15,24 @@ function App({ callback }) {
       window.close();
     }
   }  
-  const testingSet = (e) =>{ 
+  const testingSet = (e) =>{
     (globals.isTesting) ? setGlobals("isTesting", false) : setGlobals("isTesting", true);
     console.log((globals.isTesting) ? "Testing is ON" : "Testing is OFF");
   }
   return (
     <div ref={callback}>
       <h2 className='text-red-600'>Hello from React in Electron!</h2>
+      <ButtonPillBlue
+        onMouseUp={appQuit.bind(this)}
+				text="Exit"
+			/>
+      <ButtonPillBlue
+        onMouseUp={testingSet.bind(this)}
+				text="Set Testing"
+			/>
       <div id="exit-btn"
         onMouseUp={appQuit.bind(this)}
       >Exit</div>
-      <div id="logon-btn"
-        onMouseUp={testingSet.bind(this)}
-      >Set testing</div>
     </div>
   );
 }
---
+++ src/assets/Components/Shared/Buttons/PillBlue.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default ({text, onMouseUp}) => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onMouseUp={onMouseUp}>
+        {text}
+    </button>
+  )
+}
